c8df254f0b5b9dbd304809addc2eda7678d256b1
Samuel Bean <beansamuel1234@gmail.com>
2024-01-14 01:10:56 +0000

    backend created

--- src/main.js
+++ src/main.js
@@ -1,4 +1,23 @@
-const workerurl = "";
+const WebSocket = require('ws');
+let username = "greg";
+let roomCode = "greg";
 
-const websocket = new WebSocket(workerurl);
+const newWebSocket = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(new WebSocket('https://worker-chatapp.beansamuel1234.workers.dev' + "/room/" + roomCode));
+    }, 30000)})
 
+const asyncFunction = async () => {
+    websocket = await newWebSocket;
+
+    websocket.addEventListener("message", (event) => {
+        console.log(event);
+        const data = JSON.parse(event.data);
+        if (data.type = "message") {
+            console.log(data.data);
+        }
+    })
+
+    websocket.send(JSON.stringify({ type: "message", room: roomCode, user: username, data: "message" }))
+}
+asyncFunction();
--- src/webserv.js
+++ src/webserv.js
@@ -1,5 +1,9 @@
+class chatroom {
+    sessions = [];
+}
+
 export default {
-    async fetch(request, env) {
+    async fetch(request) {
         return await handleErrs(request, requestHandler);
     }
 }
@@ -11,16 +15,67 @@ async function handleErrs(request, cbfunc) {
         const upgradeHeader = request.headers.get('Upgrade');
         if (!upgradeHeader || upgradeHeader !== 'websocket') {
             return new Response('Expected Upgrade: websocket', { status: 426 });
-        } else {
-            return new Response('Server Error', { status: 500 });
         }
+        return new Response('Server Error', { status: 500 });
     }
 }
 
 async function requestHandler(request) {
+    const { method } = request;
     let url = new URL(request.url);
 
-    //add in return root functionality
+    switch(method) {
+        case 'GET':
+            return await handleGetRequest(url, request);
+        case 'UPGRADE':
+            return await handleUpgradeRequest(url, request);
+    }
+    return new Response('method not supported', { status: 405 })
+}
 
-    if (request.header)
+async function handleGetRequest(url, request) {
+    let { pathname } = url;
+    pathname = pathname.slice(1).split("/");
+    
+    if (pathname[0] == "room") {
+        //add get history function here
+        return new Response('not developed yet', { status: 404});
+    }
+    return new Response('nothing to do here', { status: 418});
+}
+
+async function handleUpgradeRequest(url, request) {
+    let { pathname } = url;
+    pathname = pathname.slice(1).split("/");
+
+    if (pathname[0] == "room") {
+        return await initializeChatRoom(url, pathname);
+    }
+}
+
+async function initializeChatRoom(url, pathname) {
+    let roomCode = pathname[1];
+
+    const webSocketPair = new WebSocketPair();
+    const client = webSocketPair[0],
+        server = webSocketPair[1];
+
+    server.accept();
+
+    chatroom.sessions.push(server);
+
+    server.on("message", async message => {
+        let data = JSON.parse(message);
+
+        for (let i = 0; i < chatroom.sessions.length; i += 1) {
+            let currSession = chatroom.sessions[i];
+            if (currSession.url.slice(1).split("/")[1] == data.room)
+            currSession.send(JSON.stringify(data));
+        }
+    })
+
+    return new Response(null, {
+        status: 101,
+        websocket: client,
+    })
 }
